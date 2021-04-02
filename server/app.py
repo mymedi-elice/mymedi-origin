4d4185edb0939d6e4f5c6255c6a56241bd2792f7
ShimJaeMin <apvmf32@gmail.com>
2021-04-02 17:11:34 +0900

    flask(초기설정, blueprint), db는 윤주님이 설정

--- server/app.py
+++ server/app.py
@@ -1,10 +1,12 @@
 from flask import Flask
+from test import test
 
 app = Flask(__name__)
+app.register_blueprint(test)
 
-@app.route("/", methods = ["GET"])
+@app.route('/', methods = ['GET'])
 def hello_world():
     return "Hello World!"
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(debug = True)
