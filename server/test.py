24c0c8da971a89bbbfa76432ff0686b06eff2087
ShimJaeMin <apvmf32@gmail.com>
2021-04-02 22:01:15 +0900

    update

--- server/test.py
+++ server/test.py
@@ -1,7 +1,17 @@
-from flask import Blueprint
+from flask import Blueprint, jsonify
+from module import db
 
 test = Blueprint("test", __name__, url_prefix = '/test')
 
 @test.route("/blueprint")
 def example():
     return "This is bluprint"
+
+# SELECT 함수 예제
+@test.route('/select', methods = ['GET'])
+def select():
+    db_class = db.Database()
+    sql = "SELECT `id`, `name` FROM `test`"
+    row = db_class.executeAll(sql)
+    print(row)
+    return jsonify(status = "success", result = row)
