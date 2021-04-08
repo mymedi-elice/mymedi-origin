10103cdb364c542be3fb5530b85f0b54103b0109
Nam Dayoung <ramda4415@gmail.com>
2021-04-08 19:58:07 +0900

    0408

--- client/mymediapp/src/pages/MyPage.js
+++ client/mymediapp/src/pages/MyPage.js
@@ -6,6 +6,7 @@ import {
   GridItem,
   HStack,
   StackDivider,
+  VStack,
 } from "@chakra-ui/layout";
 import { useContext, useState } from "react";
 import MainLayout from "../components/MainLayout";
@@ -13,22 +14,24 @@ import { logContext } from "../context";
 
 export default function MyPage() {
   const { isLoggedIn, setIsLoggedIn } = useContext(logContext);
-  const [showFavorites, setShowFavorites] = useState();
-  const [showUserInfo, setShowUserInfo] = useState();
-  const [fixUserInfo, setFixUserInfo] = useState();
-  const [signOut, setSignOut] = useState();
 
   return (
     <div>
       <MainLayout>
+        <Box>마이 페이지</Box>
         <Grid
           h="800px"
           templateRows="repeat(1, 1fr)"
           templateColumns="repeat(5, 1fr)"
+          alignItems="center"
         >
-          <GridItem rowSpan={1} colSpan={1}></GridItem>
-          <Divider orientation="vertical" />
-          <GridItem rowSpan={1} colSpan={1}></GridItem>
+          <GridItem rowSpan={1} colSpan={1}>
+            <VStack align="stretch">
+              <Box></Box>
+            </VStack>
+          </GridItem>
+          <Divider h="90%" orientation="vertical" />
+          <GridItem rowSpan={1} colSpan={4}></GridItem>
         </Grid>
       </MainLayout>
     </div>
