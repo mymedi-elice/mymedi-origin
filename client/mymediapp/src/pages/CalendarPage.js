b739e7c382d4816a1b7ef4e1e5ee7351ce270e25
Nam Dayoung <ramda4415@gmail.com>
2021-04-11 20:10:39 +0900

    fullcalendar error

--- client/mymediapp/src/pages/CalendarPage.js
+++ client/mymediapp/src/pages/CalendarPage.js
@@ -2,6 +2,12 @@ import React, { useState, useEffect, useCallback, useContext } from "react";
 import axios from "axios";
 import { serverUrl } from "../config";
 
+import FullCalendar from "@fullcalendar/react";
+import dayGridPlugin from "@fullcalendar/daygrid";
+import timeGridPlugin from "@fullcalendar/timegrid";
+import interactionPlugin from "@fullcalendar/interaction"; // needed for dayClick
+import { Box, Flex } from "@chakra-ui/react";
+
 import MainLayout from "../components/MainLayout";
 import { useTranslation } from "react-i18next";
 import useConfirmLogin from "../components/useConfirmLogin";
@@ -35,7 +41,13 @@ export default function CalendarPage() {
       isPending={isPending}
       setIsPending={setIsPending}
     >
-      {t("language")}
+      <Box maxWidth="800px" maxHeight="800px" p={20}>
+        <FullCalendar
+          plugins={[dayGridPlugin]}
+          initialView="dayGridMonth"
+          height="auto"
+        ></FullCalendar>
+      </Box>
     </MainLayout>
   );
 }
