8862c208e781f65105a9ec6bf744661fdbc14888
Nam Dayoung <ramda4415@gmail.com>
2021-04-13 13:24:24 +0900

    fix code format

--- client/mymediapp/src/App.js
+++ client/mymediapp/src/App.js
@@ -1,13 +1,10 @@
-import React, { useState, useEffect, useCallback } from "react";
+import React from "react";
 import Home from "./pages/Home";
 import MyPage from "./pages/MyPage";
 import "./App.css";
 import { Route, Switch } from "react-router-dom";
 import Introduction from "./pages/Introduction";
 import CalendarPage from "./pages/CalendarPage";
-// import { logContext } from "./context";
-// import axios from "axios";
-// import { serverUrl } from "./config";
 
 function App() {
   return (
